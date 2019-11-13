4ba916e96ef6ab9172f1ba25f59c40dcacf73cd3
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-11-13 16:05:51 -0500

    [Ramanpreet] Able to set custom controller as root view controller in scene delegate.

--- EasyGrocery/SceneDelegate.swift
+++ EasyGrocery/SceneDelegate.swift
@@ -17,7 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let window =  self.window else {return}
+        // guard let _ = (scene as? UIWindowScene) else { return }
+
+        let initalController = GroceryListWireFrame.createGroceryListModule() // Your root view controller
+        window.rootViewController = initalController
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
