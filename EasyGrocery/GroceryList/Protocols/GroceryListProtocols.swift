ed4a6d701f8b328dec027c7fe0b8df846c91e02b
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-11-13 16:07:02 -0500

    [Ramanpreet] Added protocols to achieve Protocol oriented programming.

--- EasyGrocery/GroceryList/Protocols/GroceryListProtocols.swift
+++ EasyGrocery/GroceryList/Protocols/GroceryListProtocols.swift
@@ -6,3 +6,44 @@
 //  Copyright © 2019 Ramanpreet Singh. All rights reserved.
 //
 
+import UIKit
+
+/*
+ Wireframe protocols
+ */
+protocol GroceryListWireFrameProtocol: class {
+    static func createGroceryListModule() -> UIViewController
+}
+
+/*
+ Presenter protocols
+ */
+protocol GroceryListPresenterProtocol {
+    var view: GroceryListViewProtocol? { get set }
+    var interactor: GroceryListInteractorInputProtocol? { get set }
+    var wireFrame: GroceryListWireFrameProtocol? { get set }
+    
+    // VIEW -> PRESENTER
+    func viewDidLoad()
+}
+
+/*
+Interactor protocols
+*/
+protocol GroceryListInteractorInputProtocol: class {
+    var presenter: GroceryListInteractorOutputProtocol? { get set }
+    
+    // PRESENTER -> INTERACTOR
+    func retrievePostList()
+}
+
+protocol GroceryListInteractorOutputProtocol: class {
+    
+}
+
+/*
+GroceryList view protocols
+*/
+protocol GroceryListViewProtocol: class {
+    var presenter: GroceryListPresenterProtocol? { get set }
+}
