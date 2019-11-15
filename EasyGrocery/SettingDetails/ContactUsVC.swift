663801303d1333be13518b9837538cdc89e53092
srinathballa96@gmail.com <srinathballa@1996>
2019-11-14 20:12:25 -0500

    [Srinath] vc screen

---
+++ EasyGrocery/SettingDetails/ContactUsVC.swift
@@ -0,0 +1,30 @@
+//
+//  ContactUsVC.swift
+//  EasyGrocery
+//
+//  Created by MacStudent on 2019-11-14.
+//  Copyright © 2019 Ramanpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
